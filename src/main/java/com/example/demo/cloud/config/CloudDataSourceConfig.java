ab91d3eb56d0aa41ec32285aa5999a22aad51728
Rob Edwards <rob.j.edwards@gmail.com>
2020-08-04 23:26:25 +0100

    refactor and rewrite

---
+++ src/main/java/com/example/demo/cloud/config/CloudDataSourceConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.cloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("cloud")
+public class CloudDataSourceConfig extends AbstractCloudConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+
+}
